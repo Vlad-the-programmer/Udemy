2a443e4049171cdc8524f10186a1cdc9617df98d
Vlad-the-programmer <86365591+Vlad-the-programmer@users.noreply.github.com>
2022-07-28 19:23:33 +0200

    Delete Capstone/OnlineShop_dj directory

--- Capstone/OnlineShop_dj/products_dj/products/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('products/', views.ProductRetrieveListApi.as_view(), name='product'),
-    path('register/', views.SignUpView.as_view(), name='register')
-
-]
